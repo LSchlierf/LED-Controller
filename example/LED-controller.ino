bfbe33c33e65e9d8d6bce51df25469e4b8d8feaf
LucasS01 <Lucasschlierf@gmail.com>
2022-03-19 17:04:59 +0100

    added more modes to LED controller

--- example/LED-controller.ino
+++ example/LED-controller.ino
@@ -21,12 +21,27 @@ commands:
 03: slow down
 05: set brightness
 */
-int tempo[] = {0, 20, 20, 30, 600, 40, 50};
-int tempoincrement[] = {0, 3, 3, 3, 50, 3, 5};
+/*
+modes:
+0  - off
+1  - rainbow 1
+2  - rainbow 2
+3  - bouncing strip
+4  - christmas lights
+5  - strobe light
+6  - fire brigade lights
+7  - portal
+8  - police lights
+9  - knight rider
+10 - warm white
+*/
+int tempo[] = {0, 20, 20, 30, 600, 40, 50, 30, 400, 30, 3000};
+int tempoincrement[] = {0, 3, 3, 3, 50, 3, 5, 3, 50, 3, 0};
 int last = LED_COUNT - 1;
 int length = effect_length - 1;
 int mode, lasttime, timepassed, pixel;
 float increment = 65536 / LED_COUNT;
+float position, brightness;
 bool direction;
 uint16_t color;
 uint32_t colorbuf;
@@ -50,6 +65,7 @@ void checkbluetooth() {
 
       case 1: //new mode
         mode = Bluetooth.read();
+        runAnimation();
         break;
 
       case 2: //speed up
@@ -162,6 +178,70 @@ void runAnimation() {
         strip.setPixelColor(pos, 0, 0, 255);
       }
 
+      break;
+
+    case 7: //portal
+      for (int i = 0; i < LED_COUNT; i++){
+        brightness = position + i;
+        if (brightness >= LED_COUNT){
+          brightness -= LED_COUNT;
+        }
+        if (brightness >= (LED_COUNT / 2)){
+          brightness -= (LED_COUNT / 2);
+          brightness = 255 - brightness;
+          brightness = map(brightness, 235, 255, 0, 255);
+          strip.setPixelColor(i, brightness, (brightness / 3), 0);
+        }
+        else{
+          brightness = 255 - brightness;
+          brightness = map(brightness, 235, 255, 0, 255);
+          strip.setPixelColor(i, 0, 0, brightness);
+        }
+      }
+      position++;
+      if (position > LED_COUNT){
+        position -= LED_COUNT;
+      }
+      break;
+
+    case 8: //police lights
+      direction = !direction;
+      for(int i = 0; i < LED_COUNT; i += effect_length){
+        if((i%(effect_length * 2) == 0) == direction){
+          for(int j = 0; j < effect_length; j++){
+            strip.setPixelColor((i + j), strip.Color(255, 0, 0));
+          }
+        }
+        else {
+          for(int j = 0; j < effect_length; j++){
+            strip.setPixelColor((i + j), strip.Color(0, 0, 255));
+          }
+        }
+      }
+      break;
+
+    case 9: //knight rider
+      for(int i = 0; i < effect_length; i++){
+        strip.setPixelColor((position + i), 255, 0, 0);
+      }
+      if(direction){
+        position++;
+        if(position >= LED_COUNT - effect_length){
+          direction = !direction;
+        }
+      }
+      else{
+        position--;
+        if(position <= 0){
+          direction = !direction;
+        }
+      }
+      break;
+
+    case 10: //warm white
+      for(int i = 0; i < LED_COUNT; i++){
+        strip.setPixelColor(i, 255, 120, 0);
+      }
       break;
       
   }
